2dcabb4ac18042e3151d8d37ea76ebee73d666c4
sts_chung_nguyen <chung.nguyen@saigontechnology.com>
2019-11-12 15:15:59 +0700

    Add project files.

---
+++ PublishBasedOnEnv/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PublishBasedOnEnv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
